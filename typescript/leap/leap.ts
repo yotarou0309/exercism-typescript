58bab6cfde5f0de32b1834ade79987046f91a010
Your Name <your.email@example.com>
2026-06-21 06:30:58 +0000

    Line Upの環境を構築

--- typescript/leap/leap.ts
+++ typescript/leap/leap.ts
@@ -1,3 +1,11 @@
-export function isLeap() {
-  throw new Error('Remove this line and implement the function')
+export function isLeap(year: number): boolean {
+  const isDivisibleBy4 = isDivisibleBy(4);
+  const isDivisibleBy100 = isDivisibleBy(100);
+  const isDivisibleBy400 = isDivisibleBy(400);
+
+  return isDivisibleBy4(year) && (!isDivisibleBy100(year) || isDivisibleBy400(year))
+}
+
+function isDivisibleBy(num: number): (n: number) => boolean {
+  return (n: number) => n % num === 0
 }
