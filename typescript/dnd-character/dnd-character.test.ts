cb7f7810374b72ef93885c54a2c812c6cd8e5107
Your Name <your.email@example.com>
2026-06-25 08:48:04 +0000

    DnDCharacter提出

--- typescript/dnd-character/dnd-character.test.ts
+++ typescript/dnd-character/dnd-character.test.ts
@@ -1,4 +1,4 @@
-import { describe, it, expect, xit } from '@jest/globals'
+import { describe, it, expect } from '@jest/globals'
 import { DnDCharacter } from './dnd-character.ts'
 
 describe('Ability modifier', () => {
@@ -6,69 +6,69 @@ describe('Ability modifier', () => {
     expect(DnDCharacter.getModifierFor(3)).toEqual(-4)
   })
 
-  xit('Ability modifier for score 4 is -3', () => {
+  it('Ability modifier for score 4 is -3', () => {
     expect(DnDCharacter.getModifierFor(4)).toEqual(-3)
   })
 
-  xit('Ability modifier for score 5 is -3', () => {
+  it('Ability modifier for score 5 is -3', () => {
     expect(DnDCharacter.getModifierFor(5)).toEqual(-3)
   })
 
-  xit('Ability modifier for score 6 is -2', () => {
+  it('Ability modifier for score 6 is -2', () => {
     expect(DnDCharacter.getModifierFor(6)).toEqual(-2)
   })
 
-  xit('Ability modifier for score 7 is -2', () => {
+  it('Ability modifier for score 7 is -2', () => {
     expect(DnDCharacter.getModifierFor(7)).toEqual(-2)
   })
 
-  xit('Ability modifier for score 8 is -1', () => {
+  it('Ability modifier for score 8 is -1', () => {
     expect(DnDCharacter.getModifierFor(8)).toEqual(-1)
   })
 
-  xit('Ability modifier for score 9 is -1', () => {
+  it('Ability modifier for score 9 is -1', () => {
     expect(DnDCharacter.getModifierFor(9)).toEqual(-1)
   })
 
-  xit('Ability modifier for score 10 is 0', () => {
+  it('Ability modifier for score 10 is 0', () => {
     expect(DnDCharacter.getModifierFor(10)).toEqual(0)
   })
 
-  xit('Ability modifier for score 11 is 0', () => {
+  it('Ability modifier for score 11 is 0', () => {
     expect(DnDCharacter.getModifierFor(11)).toEqual(0)
   })
 
-  xit('Ability modifier for score 12 is 1', () => {
+  it('Ability modifier for score 12 is 1', () => {
     expect(DnDCharacter.getModifierFor(12)).toEqual(1)
   })
 
-  xit('Ability modifier for score 13 is 1', () => {
+  it('Ability modifier for score 13 is 1', () => {
     expect(DnDCharacter.getModifierFor(13)).toEqual(1)
   })
 
-  xit('Ability modifier for score 14 is 2', () => {
+  it('Ability modifier for score 14 is 2', () => {
     expect(DnDCharacter.getModifierFor(14)).toEqual(2)
   })
 
-  xit('Ability modifier for score 15 is 2', () => {
+  it('Ability modifier for score 15 is 2', () => {
     expect(DnDCharacter.getModifierFor(15)).toEqual(2)
   })
 
-  xit('Ability modifier for score 16 is 3', () => {
+  it('Ability modifier for score 16 is 3', () => {
     expect(DnDCharacter.getModifierFor(16)).toEqual(3)
   })
 
-  xit('Ability modifier for score 17 is 3', () => {
+  it('Ability modifier for score 17 is 3', () => {
     expect(DnDCharacter.getModifierFor(17)).toEqual(3)
   })
 
-  xit('Ability modifier for score 18 is 4', () => {
+  it('Ability modifier for score 18 is 4', () => {
     expect(DnDCharacter.getModifierFor(18)).toEqual(4)
   })
 })
 
 describe('Ability generator', () => {
-  xit('Random ability is within range', () => {
+  it('Random ability is within range', () => {
     const abilityScore = DnDCharacter.generateAbilityScore()
     expect(abilityScore).toBeGreaterThanOrEqual(3)
     expect(abilityScore).toBeLessThanOrEqual(18)
@@ -76,7 +76,7 @@ describe('Ability generator', () => {
 })
 
 describe('Character creation', () => {
-  xit('Random character is valid - hitpoints', () => {
+  it('Random character is valid - hitpoints', () => {
     const character = new DnDCharacter()
 
     expect(character.hitpoints).toEqual(
@@ -84,49 +84,49 @@ describe('Character creation', () => {
     )
   })
 
-  xit('Random character is valid - strength', () => {
+  it('Random character is valid - strength', () => {
     const character = new DnDCharacter()
 
     expect(character.strength).toBeGreaterThanOrEqual(3)
     expect(character.strength).toBeLessThanOrEqual(18)
   })
 
-  xit('Random character is valid - dexterity', () => {
+  it('Random character is valid - dexterity', () => {
     const character = new DnDCharacter()
 
     expect(character.dexterity).toBeGreaterThanOrEqual(3)
     expect(character.dexterity).toBeLessThanOrEqual(18)
   })
 
-  xit('Random character is valid - constitution', () => {
+  it('Random character is valid - constitution', () => {
     const character = new DnDCharacter()
 
     expect(character.constitution).toBeGreaterThanOrEqual(3)
     expect(character.constitution).toBeLessThanOrEqual(18)
   })
 
-  xit('Random character is valid - intelligence', () => {
+  it('Random character is valid - intelligence', () => {
     const character = new DnDCharacter()
 
     expect(character.intelligence).toBeGreaterThanOrEqual(3)
     expect(character.intelligence).toBeLessThanOrEqual(18)
   })
 
-  xit('Random character is valid - wisdom', () => {
+  it('Random character is valid - wisdom', () => {
     const character = new DnDCharacter()
 
     expect(character.wisdom).toBeGreaterThanOrEqual(3)
     expect(character.wisdom).toBeLessThanOrEqual(18)
   })
 
-  xit('Random character is valid - charisma', () => {
+  it('Random character is valid - charisma', () => {
     const character = new DnDCharacter()
 
     expect(character.charisma).toBeGreaterThanOrEqual(3)
     expect(character.charisma).toBeLessThanOrEqual(18)
   })
 
-  xit('Each ability is only calculated once', () => {
+  it('Each ability is only calculated once', () => {
     const character = new DnDCharacter()
 
     expect(character.strength === character.strength).toBeTruthy()
--- typescript/dnd-character/dnd-character.ts
+++ typescript/dnd-character/dnd-character.ts
@@ -1,9 +1,34 @@
 export class DnDCharacter {
   public static generateAbilityScore(): number {
-    throw new Error('Remove this line and implement the function')
+    return randomInt(3, 18)
   }
 
   public static getModifierFor(abilityValue: number): number {
-    throw new Error('Remove this line and implement the function')
+    const s = (abilityValue - 10) / 2
+    return Math.floor(s);
   }
+
+  strength:number
+  dexterity: number
+  intelligence: number
+  wisdom: number
+  charisma: number
+  constitution: number
+  hitpoints: number
+
+  constructor(
+  ) {
+      this.strength = DnDCharacter.generateAbilityScore()
+      this.dexterity = DnDCharacter.generateAbilityScore()
+      this.intelligence = DnDCharacter.generateAbilityScore()
+      this.wisdom = DnDCharacter.generateAbilityScore()
+      this.charisma = DnDCharacter.generateAbilityScore()
+      this.constitution = DnDCharacter.generateAbilityScore()
+      this.hitpoints = 10 + DnDCharacter.getModifierFor(this.constitution);
+  }
+
+}
+
+function randomInt(min: number, max: number): number {
+  return Math.floor(Math.random() * (max - min + 1)) + min
 }
